dd1eecc1cb2a61e2373aff872800327c49f964fd
r12f <r12f.code@gmail.com>
2017-08-09 19:48:35 -0700

    Initial commit.

---
+++ index.d.ts
@@ -0,0 +1,6 @@
+﻿interface RoomVisual {
+    structure(x: number, y: number, type: string, opts?: { opacity?: number });
+    connectRoads(opts?: { opacity?: number, color?: string });
+    animatedPosition(x: number, y: number, opts?: { color?: string, opacity?: number, radius?: number, frames: number });
+    speech(text: string, x: number, y: number, opts?: { background?: string, textcolor?: string, textstyle?: string, textsize?: number, textfont?: string, opacity?: number });
+}
